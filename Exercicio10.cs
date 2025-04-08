36bb33b952ba5700b4e26d89058fc50a0a4b5531
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-04-08 20:41:29 -0300

    Add files via upload

---
+++ Exercicio10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um texto:");
+        string texto = Console.ReadLine();
+
+        Console.WriteLine("Digite a palavra que deseja buscar:");
+        string palavra = Console.ReadLine();
+
+        int posicao = BuscarPalavraNoTexto(texto, palavra);
+
+        if (posicao != -1)
+        {
+            Console.WriteLine($"A palavra '{palavra}' foi encontrada na posição {posicao}.");
+        }
+        else
+        {
+            Console.WriteLine("Palavra não encontrada no texto.");
+        }
+    }
+
+    static int BuscarPalavraNoTexto(string texto, string palavra)
+    {
+        return texto.IndexOf(palavra, StringComparison.OrdinalIgnoreCase);
+    }
+}
