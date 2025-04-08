5dd7771605be0ef08c8883f0c1d2a9c5fb8cff4d
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-04-08 20:40:22 -0300

    Add files via upload

---
+++ Exercicio8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 3, 5, 77, 32, 13, 2, 44 };
+
+        int numeroPar = BuscarPrimeiroPar(numeros);
+
+        if (numeroPar != -1)
+        {
+            Console.WriteLine($"O primeiro número par encontrado é: {numeroPar}");
+        }
+        else
+        {
+            Console.WriteLine("Nenhum número par foi encontrado no array.");
+        }
+    }
+
+    static int BuscarPrimeiroPar(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            {
+                return array[i];
+            }
+        }
+
+        return -1; 
+    }
+}
