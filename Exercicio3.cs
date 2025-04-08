f796c6367d83e0e0ddf0d434a064f9fa6c6f8ac5
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-04-08 20:37:50 -0300

    Add files via upload

---
+++ Exercicio3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System;
+class Program
+{
+    static void Main()
+    {
+        string[] nomes = { "Rafael", "Bruno", "Maria Rita", "Matheus", "Vinicius", "Filipe", "Arthur" };
+
+        Console.WriteLine("Digite o nome que você quer buscar (Lembre-se: a primeira letra deve ser maiuscula):");
+        string nomeProcurado = Console.ReadLine();
+
+        int indice = BuscarNome(nomes, nomeProcurado);
+
+        if (indice != -1)
+        {
+            Console.WriteLine($"Nome '{nomeProcurado}' encontrado no índice {indice}.");
+        }
+        else
+        {
+            Console.WriteLine($"Nome '{nomeProcurado}' não encontrado na lista.");
+        }
+    }
+
+    static int BuscarNome(string[] array, string nome)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Equals(nome))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
