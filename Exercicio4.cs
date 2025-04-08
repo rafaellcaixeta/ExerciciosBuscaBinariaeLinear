a4c10c76853c6288cd2e7c21a4e4db334b35a8c6
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-04-08 20:38:20 -0300

    Add files via upload

---
+++ Exercicio4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Escreva quantos números inteiros desejar:");
+        int[] numeros = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int maior = EncontrarMaior(numeros);
+        int menor = EncontrarMenor(numeros);
+
+        Console.WriteLine($"O maior número encontrado é: {maior}");
+        Console.WriteLine($"O menor número encontrado é: {menor}");
+    }
+
+    static int EncontrarMaior(int[] array)
+    {
+        int maior = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > maior)
+            {
+                maior = array[i];
+            }
+        }
+
+        return maior;
+    }
+
+    static int EncontrarMenor(int[] array)
+    {
+        int menor = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < menor)
+            {
+                menor = array[i];
+            }
+        }
+
+        return menor;
+    }
+}
